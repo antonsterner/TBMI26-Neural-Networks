a1857dcbfe7d903b339d0b7715397cdd0b75927f
Anton Sterner <anton.sterner@gmail.com>
2018-05-05 14:57:14 +0200

    lab4 fix

--- TBMI26_assignments/Qlearning/main.m
+++ TBMI26_assignments/Qlearning/main.m
@@ -14,19 +14,26 @@ Q = rand(state.xsize, state.ysize, 4);
 actions = [1 2 3 4];
 % The optimal action is chosen with probability (1-eps), 
 % otherwise a random action is chosen
-eps = 0.8;
+eps = 0.9;
 epsorg = eps;
+epsmin = 0.05;
 % probability for each action
 prob_a = 0.25*[1 1 1 1];
 % learning rate
-alpha = 0.3;
+alpha = 0.1;
 % discount factor
-dc_factor = 0.95;
+dc_factor = 0.9;
 % k = number of episodes
-k=5000;
+k=10000;
+
 %%
 % for each episode
 for i=1:k
+    % print progress
+    if(rem(i,100) == 0)
+       i
+       eps
+    end
     % initialize grid world
     gwinit(worldnr)
     % find initial state
@@ -48,30 +55,19 @@ for i=1:k
             Q(state.pos(1), state.pos(2), action) = (1-alpha)*Q(state.pos(1), state.pos(2), action) ...
             + alpha * (r + dc_factor * max(Q(next_state.pos(1),next_state.pos(2),:)));
             state = next_state;
-        else
-            % Prevent punishing wrong random moves by comparing the
-            % direction it moved, and the action taken
-            % what direction did it move
-            xdiff = next_state.pos(1) - state.pos(1);
-            ydiff = next_state.pos(2) - state.pos(2);
-            % does the change in position match the taken action
-            down = (xdiff == 1 && action == 1);
-            up = (xdiff == -1 && action == 2);
-            right = (ydiff == 1 && action == 3);
-            left = (ydiff == -1 && action == 4);
-            % if move was genuine
-            if(down || up || right || left)
-                Q(state.pos(1), state.pos(2), action) = -inf; 
-            else % if random action was taken
-                r = -1;
-                Q(state.pos(1), state.pos(2), action) = (1-alpha)*Q(state.pos(1), state.pos(2), action) ...
-                + alpha * (r + dc_factor * max(Q(next_state.pos(1),next_state.pos(2),:)));
-            end
+        else 
+            r = -0.1; % more negative feedback from moving into a wall
+            Q(state.pos(1), state.pos(2), action) = (1-alpha)*Q(state.pos(1), state.pos(2), action) ...
+            + alpha * (r + dc_factor * max(Q(next_state.pos(1),next_state.pos(2),:)));
         end
     end
-    eps = eps - epsorg/k; % reduce exploration rate
+    if(eps > epsmin)
+        eps = eps - epsorg/k; % reduce exploration rate
+    end
+    % reward for moving from end tile = 0
+    Q(state.pos(1),state.pos(2),:) = 0;
 end
-
+%%
 figure(1)
 gwdraw();
 for x = 1:state.xsize
@@ -81,14 +77,14 @@ for x = 1:state.xsize
    end
 end
 % plot Q values
-figure(2)
-imagesc(Q(:,:,1))
-
-figure(3)
-imagesc(Q(:,:,2))
-
-figure(4)
-imagesc(Q(:,:,3))
-
-figure(5)
-imagesc(Q(:,:,4))
+% figure(2)
+% imagesc(Q(:,:,1))
+% 
+% figure(3)
+% imagesc(Q(:,:,2))
+% 
+% figure(4)
+% imagesc(Q(:,:,3))
+% 
+% figure(5)
+% imagesc(Q(:,:,4))
